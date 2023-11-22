43472a4a78a9ee115b914143e1965398a3a78f68
ayundhasachi <ayundhamulia@gmail.com>
2023-11-22 10:09:44 +0700

    TUGAS9

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,10 @@
-from django.urls import path
+from django.urls import include, path
+from authentication.views import logout
 from main.views import add_product_ajax, delete_product_ajax, get_product_json, show_main
 from main.views import show_main, create_product
 from main.views import show_main, create_product, show_xml 
 from main.views import show_main, create_product, show_xml, show_json
-from main.views import show_main, create_product, show_xml, show_json, show_xml_by_id, show_json_by_id 
+from main.views import show_main, create_product_flutter, show_xml, show_json, show_xml_by_id, show_json_by_id 
 from main.views import register #sesuaikan dengan nama fungsi yang dibuat
 from main.views import login_user #sesuaikan dengan nama fungsi yang dibuat
 from main.views import logout_user
@@ -26,5 +27,7 @@ urlpatterns = [
     path('delete/<int:id>', delete_product, name='delete_product'),
     path('get-product/', get_product_json, name='get_product_json'),
     path('create-product-ajax/', add_product_ajax, name='add_product_ajax'),
-    path('delete-product-ajax/<int:id>', delete_product_ajax, name='delete_product_ajax')
+    path('delete-product-ajax/<int:id>', delete_product_ajax, name='delete_product_ajax'),
+    path('create-flutter/', create_product_flutter, name='create_product_flutter'),
+    path('logout/', logout, name='logout'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,6 +1,6 @@
 import json
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponseNotFound, HttpResponseRedirect
+from django.http import HttpResponseNotFound, HttpResponseRedirect, JsonResponse
 from main.forms import ProductForm
 from django.urls import reverse
 from django.http import HttpResponse
@@ -142,3 +142,22 @@ def delete_product_ajax(request, id):
         product.delete()
         return HttpResponse(b"DELETED", status=200)
     return HttpResponseNotFound()
+
+@csrf_exempt
+def create_product_flutter(request):
+    if request.method == 'POST':
+        
+        data = json.loads(request.body)
+
+        new_product = Product.objects.create(
+            user = request.user,
+            name = data["name"],
+            price = int(data["price"]),
+            description = data["description"]
+        )
+
+        new_product.save()
+
+        return JsonResponse({"status": "success"}, status=200)
+    else:
+        return JsonResponse({"status": "error"}, status=401)
